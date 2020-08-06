45d6a036211699407d422ee572b2d8fd73080104
buriburi <sagarshayr999@gmail.com>
2020-08-06 12:44:52 +0530

    abhi maine language and project dono pages bnaa liye and maine sarre link jod diye hai

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from main import views
 urlpatterns = [
-    path('',views.index)
+    path('',views.index,name='index'),
+    path('project',views.project,name='project'),
+    path('language',views.language,name='language'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
 def index(request):
+    developer='Sagar Shivam Kaushik'
+    d={'developer':developer}
+    return render(request,'main/index.html',d)
+def project(request):
     d=dict()
-    return render(request,'main\index.html',d)
+    return render(request,'main/project.html',d)
+def language(request):
+    d=dict()
+    return render(request,'main/language.html',d)
